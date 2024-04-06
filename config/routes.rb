2568228653c1feefdd395eded733d6e2132e250f
Gerardo Calderon <gerardoh@calderonlopez.com>
2024-04-05 20:44:30 -0500

    Adding routes home, about, and contact

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root to: "pages#home"
+
+  # GET localhost:3000/about
+  get "/about", to: "pages#about"
+
+  get "/contact", to: "pages#contact"
 end
